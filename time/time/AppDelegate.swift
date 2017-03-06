6e4ad904852a1c9dd23c04fb1af46345c323582d
Sohil Pandya <sohilpandya@Sohils-MBP.lan>
2017-03-06 16:01:23 +0000

    added ability to view tasks implemented nav-bar, table view related to #6

--- time/time/AppDelegate.swift
+++ time/time/AppDelegate.swift
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //changing status bar style
+        
+        UIApplication.shared.statusBarStyle = .lightContent
         return true
     }
 
